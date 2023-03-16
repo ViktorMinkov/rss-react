1d11bfb2fb4c560d7b7f8b834fa6f162fdc66fe9
Viktor <minkovvs@gmail.com>
2023-03-16 19:47:37 +0300

    feat: add configs for testing

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,14 +1,17 @@
 import Card from 'components/Card';
 import React from 'react';
 import data from 'utils/data';
+import './CardList.scss';
 
 class CardList extends React.Component {
   render() {
     return (
       <>
-        {data.map((character) => (
-          <Card key={character.id} character={character} />
-        ))}
+        {data.length > 0 ? (
+          data.map((character) => <Card key={character.id} character={character} />)
+        ) : (
+          <div className="card-list_empty">No data</div>
+        )}
       </>
     );
   }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -7,4 +8,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: false,
+  },
 });
