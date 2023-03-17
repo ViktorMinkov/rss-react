8e4ec2dcbdc86681d4c0449b869c4e395ed8f650
Viktor <minkovvs@gmail.com>
2023-03-17 18:07:04 +0300

    feat:update test in App component

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,41 +2,11 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from 'pages/About';
-import ErrorPage from 'pages/ErrorPage';
-import Home from 'pages/Home';
 
 describe('App test', () => {
-  test('render Home page', () => {
-    render(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
-    );
+  test('render App', () => {
+    render(<App />);
     expect(screen.getByText(/home page/i)).toBeInTheDocument();
-  });
-  test('render About page', () => {
-    render(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<About />}></Route>
-        </Routes>
-      </BrowserRouter>
-    );
-    expect(screen.getByText(/about page/i)).toBeInTheDocument();
-  });
-  test('render Error page', () => {
-    render(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ErrorPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    );
-    expect(screen.getByText(/page not found/i)).toBeInTheDocument();
   });
   test('find home title', () => {
     render(<App />);
