19e171e4ebe69c065f9c93c9ca9e4894f1065fad
Viktor <minkovvs@gmail.com>
2023-04-02 20:06:15 +0300

    feat: add test to input date

--- src/components/InputDate/InputDate.test.tsx
+++ src/components/InputDate/InputDate.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { describe, test, expect, vi } from 'vitest';
 import InputDate from './InputDate';
+import userEvent from '@testing-library/user-event';
 
 describe('InputDate test', () => {
   const register = vi.fn();
@@ -11,5 +12,11 @@ describe('InputDate test', () => {
 
   test('render InputDate component', () => {
     expect(screen.getByText(/date/i)).toBeInTheDocument();
+  });
+  test('check date input works', async () => {
+    const dateInput = screen.getByRole<HTMLInputElement>('datePicker');
+    const testDate = '1993-01-13';
+    await userEvent.type(dateInput, testDate);
+    expect(dateInput.value).toBe(testDate);
   });
 });
